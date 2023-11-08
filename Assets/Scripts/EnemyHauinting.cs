49f466be57478e43075e6e2debf3ee1c001fbb92
lagushonok2000 <vorobjeva.an4stasiy@yandex.ru>
2023-11-09 00:21:16 +0300

    09.11.23

---
+++ Assets/Scripts/EnemyHauinting.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHauinting : MonoBehaviour
+{
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private NavMeshAgent _agent;
+
+    private void Update()
+    {
+        _agent.SetDestination(_playerTransform.position);
+    }
+}
